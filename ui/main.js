bf67fca2fcd249dfdfa3f74b9e7700cec566b534
nvivek65 <nvivek65@gmail.com>
2017-08-17 11:22:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,15 +29,34 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-  // Make the request to the server and send the name 
+    //Create a request object
+  var request = new XMLHttpRequest();
   
-  // Capture the list of names and render it as a list.
-  var names = ['name1','name2','name3','name4'];
+  
+  //Capture the response and store it in a variable
+  request.onreadstatechange = function () {
+    if (request.readyState === XMLHttprequest.DONE){
+        //Take some action
+        if(request.status === 200){
+        // Capture the list of names and render it as a list.
+  var names = request.responseText;
+  names =JSON.parse(names);
   var list = '';
   for(var i=0;i<names.length;i++){
       list += '<li>' + names[i] + '</li>';
   }
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;
+                    }
+        
+    }
+       //Not done yet
+  };
+  //make the request
+  request.open('GET','http://nvivek65.imad.hasura-app.io/subit-name?name=' + name,true);
+  request.send(null);
+  // Make the request to the server and send the name 
+  
+  
   
 };
