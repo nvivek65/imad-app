71236b22abfd81497890ceb297ea47328315c4d6
nvivek65 <nvivek65@gmail.com>
2017-08-19 11:18:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'nvivek65',
+    database: 'nvivek65',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -43,6 +52,7 @@ var articles = {
           </p>`
 }
 };
+
 function createTemplate (data) {
 var title = data.title;
 var date = data.date;
@@ -83,7 +93,20 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
- 
+
+var pool = new Pool(config); 
+app.get('/test-db', function(req, res){
+  //make a select request
+  //return a response with the results
+  pool.query('SELECT * FROM test',function(err, result){
+     if(err){
+         res.status(500).send(err.toString());
+     } else {
+         res.send(JSON.stringify(result));
+     }
+  });
+});
+
 var counter =0;
 app.get('/counter',function(req, res){
     counter = counter+1;
