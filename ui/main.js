8a00fb8c819b5d9d643a765b10b3785e17092a1a
nvivek65 <nvivek65@gmail.com>
2017-08-23 12:30:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-//Counter Code
-var button = document.getElementById('counter');
-button.onclick = function () {
-  //Create a request object
-  var request = new XMLHttpRequest();
-  
-  
-  //Capture the response and store it in a variable
-  request.onreadstatechange = function () {
-    if (request.readyState === XMLHttprequest.DONE){
-        //Take some action
-        if(request.status === 200){
-            var counter = request.responseText
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-        
-    }
-       //Not done yet
-  };
-  //make the request
-  request.open('GET','http://nvivek65.imad.hasura-app.io/counter',true);
-  request.send(null);
-  
-};
-
-// Submit Name
+// Submit Username/password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -37,25 +11,25 @@ submit.onclick = function() {
     if (request.readyState === XMLHttprequest.DONE){
         //Take some action
         if(request.status === 200){
-        // Capture the list of names and render it as a list.
-  var names = request.responseText;
-  names =JSON.parse(names);
-  var list = '';
-  for(var i=0;i<names.length;i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
-                    }
+        console.log('user logged in');
+        alert('Logged in successfully');
+            } else if  (request.status === 403) {
+                alert('Username/password is incorrect');
+            } else if(request.status === 500) {
+                alert('Something went wrong on the server');
+            }
         
     }
        //Not done yet
   };
   //make the request
-  var nameInput = document.getElementById('name');
-  var name = nameInput.value;
-  request.open('GET','http://nvivek65.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
+  request.open('POST','http://nvivek65.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username: username, password: password}));
   // Make the request to the server and send the name 
 
 };
